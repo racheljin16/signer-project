d6e5b75f540a88a5160365b7402ebd8207df0037
Jeremy <jerepeck@gmail.com>
2016-04-26 16:50:42 -0700

    update following 1st class presentation (4/26/16)

--- js/quiz-page.js
+++ js/quiz-page.js
@@ -1,92 +1,110 @@
 $(document).ready(function() {	
-	// var chart = new Highcharts.Chart()
-	// {
-				// chart: {
-				// 		renderTo: 'chartcontainer',
-				// 		type: 'column',
-				// 	},
-				// 	title: {
-				// 		text: 'Quiz Results'
-				// 	},
-				// 	xAxis: {
-				// 		categories: ['Correct', 'Incorrect']
-				// 	},
-				// 	yAxis: {
-				// 		title: 'Response'
-				// 		},
-				// 		stackLables: {
-				// 			enabled: true,
-				// 		},
-				// 		plotOptions: {
-				// 			column: {
-				//                 stacking: 'normal',
-				// 				}
-				// 		},
 
-				// 	series: [{
-				// 		name: 'Mary',
-				// 		data: [1,3,6]
-				// 	}, {
-				// 		name: 'Bob',
-				// 		data: [5,7,2]
-				// 	}]
-				// });
+	document.getElementById("hint1").innerHTML = "Incorrect order";
+	document.getElementById("hint3").innerHTML = "Incorrect order";
+	var sortCounter1 = 0;
+	var sortCounter3 = 0;
+	document.getElementById("moves1").innerHTML = "--";
+	document.getElementById("moves3").innerHTML = "--";
+
+	var orderQuiz1 = [1.3,1.6,1.9];
+	var orderQuiz3 = [3.1,3.2,3.3];
+	var notifyIncorrect = "Not yet, keep trying.";
+	var notifiyCorrect = "That's correct."
+
+	
+	var randomNumber = Math.floor(Math.random() * 3) + 1;
+	console.log(randomNumber);
+	if (randomNumber === 1) {
+		document.getElementById("randomWord").innerHTML = "PLAY";
+		$(".play-img").addClass('valid');
+	} else if (randomNumber === 2) {
+		document.getElementById("randomWord").innerHTML = "MORE";
+		$(".more-img").addClass('valid');
+	} else {
+		document.getElementById("randomWord").innerHTML = "SAME";
+		$(".same-img").addClass('valid');
+	}
+
+	// var draggableID = $(".valid").attr("id");
+	// console.log(draggableID);
+
 
 	$(".sortables-1").sortable( {
-			containment: ".quiz1-container"
-		});
+		containment: ".quiz1-container",
+		revert: true,
+
+		update: function( event, ui ) {			
+			var sortedIDsQuiz1 = $(this).sortable("toArray").toString();
+
+			if ( sortedIDsQuiz1 == orderQuiz1 ) {
+				sortCounter1++;
+				document.getElementById("moves1").innerHTML = sortCounter1;
+				// console.log(sortedIDsQuiz1, orderQuiz1);
+				document.getElementById("hint1").innerHTML = notifiyCorrect;
+				$( ".sortables-1" ).sortable( "disable" );
+				setLocalStorate("Q1Moves",sortCounter1);
+			} else {
+				sortCounter1++;
+				document.getElementById("moves1").innerHTML = sortCounter1;				
+				// console.log(sortedIDsQuiz1, orderQuiz1);
+				document.getElementById("hint1").innerHTML = notifyIncorrect;
+				setLocalStorate("Q1Moves",sortCounter1);
+			};
+		}
+	});
 
 	$(".draggable-2").draggable( {
-			containment: ".quiz2-container"
+			containment: ".quiz2-container",
 		});
 
-	$(".droppable-2").droppable();
+	$(".droppable-2").droppable({
+		drop: function( event, ui ) {
+
+			var draggableID = $(".valid").attr("id");
+			console.log(draggableID);
+
+			// if ( randomNumber == id of draggable )
+		}
+	});
 
 	$(".sortables-3").sortable( {
-			containment: ".quiz3-container"
-		});
+		containment: ".quiz3-container",
+		revert: true,
 
-	// (".selectable").selectable();
-		// {
-		// 	tolerance: "fit",
-		// 	drop: function( event, ui ) {
+		update: function( event, ui ) {
+			var sortedIDsQuiz3 = $(this).sortable("toArray").toString();
 
-		// 		$(ui.draggable).addClass("dropped");
-		// 		var currentCount = $(".dropped").length;	
-
-		// 		count++;
-		// 		console.log(count,$(".draggables").length);
-
-		// 		if ( count != $(".draggables").length ) {
-		// 			document.getElementById('instructions1').innerHTML = countdown + " more to go";
-		// 			countdown--;
-		// 		} else {
-		// 			$("#example1").hide();
-		// 			document.getElementById('onedown').innerHTML = "<h2>Good job!<br>Move on to the next example.</h2>";
-		// 			$("#example2").removeClass("hidden");
-		// 			$("#example2").addClass("visible");
-		// 			}
-		// 		}
-		// }
-
+			if ( sortedIDsQuiz3 == orderQuiz3 ) {
+				sortCounter3++;
+				document.getElementById("moves3").innerHTML = sortCounter3;				
+				// console.log(sortedIDsQuiz3, orderQuiz3, notifiyCorrect);
+				document.getElementById("hint3").innerHTML = notifiyCorrect;
+				$( ".sortables-3" ).sortable( "disable" );
+			} else {
+				sortCounter3++;
+				document.getElementById("moves3").innerHTML = sortCounter3;				
+				// console.log(sortedIDsQuiz3, orderQuiz3, notifyIncorrect);
+				document.getElementById("hint3").innerHTML = notifyIncorrect;
+			};
+		}
+	});
 });
-	// {
-	// 	sort: function ( event, ui ) {
 
-	// 	var sortorder = $("#sortable").sortable('toArray');
+	function incorrectResponse4() {
+		document.getElementById("outcome4").innerHTML = '<h3>That is incorrect. Perhaps you should go back to <a href="expression.html">Learn</a> more.<h3>';
+		document.getElementById("quiz-btn1").disabled = true;
+		document.getElementById("quiz-btn2").disabled = true;
+		document.getElementById("quiz-btn3").disabled = true;
+	}
 
-	// 		if ( sortorder != "li2", "li3", "li4", "li5", "li6", "li7" ) {
-	// 			document.getElementById('instructions3').innerHTML = "Sort the following rows from the lightest on top to the darkest on the bottom.<br>";
-	// 				console.log(sortorder);
+	function correctResponse4() {
+		document.getElementById("outcome4").innerHTML = '<h3>Correct, great job!<h3>';
+		document.getElementById("quiz-btn1").disabled = true;
+		document.getElementById("quiz-btn2").disabled = true;
+		document.getElementById("quiz-btn3").disabled = true;
+	}
 
-	// 		} else {
-	// 			document.getElementById('instructions3').innerHTML = "Way to go!  You are all done.<br>";
-	// 			$("#example3").removeClass("visible");
-	// 			$("#example3").addClass("hidden");
-	// 		}
 
-	// 	}
-
-	// });
 
 	
--- js/signup.js
+++ js/signup.js
@@ -1,36 +1,43 @@
 $(function(){
 
-	
-	 
+  
+   
 
     $("#signup-submit").click(function() {
-    	var myemail = $("#username").val();
-    	var mypassword = $("#password").val();
-    	console.log(email, password);
+      var myemail = $("#username").val();
+      var mypassword = $("#password").val();
+      console.log(email, password);
 
-    	var ref = new Firebase("https://blinding-fire-5487.firebaseio.com/?page=Auth");
-    	ref.createUser({
- 			 email    :  myemail,
- 			 password : mypassword
-		}, function(error, userData) {
-  			if (error) {
-    			console.log("Error creating user:", error);
-  			} else {
-    			console.log("Successfully created user account with uid:", userData.uid);
+      var ref = new Firebase("https://blinding-fire-5487.firebaseio.com/?page=Auth");
+      ref.createUser({
+       email    :  myemail,
+       password : mypassword
+    }, function(error, userData) {
+        if (error) {
+          console.log("Error creating user:", error);
+        } else {
+          console.log("Successfully created user account with uid:", userData.uid);
           localStorage.setItem("Greeting"," back," + myemail);
-    			window.location.href="index.html";
-  		}
-		}); 
-		return false;
-	});
+          localStorage.setItem("Quiz1", "NA");
+          localStorage.setItem("Quiz2", "NA");
+          localStorage.setItem("Quiz3", "NA");
+          localStorage.setItem("Quiz4", "NA");
+          localStorage.setItem("Q1Moves", 0);
+          localStorage.setItem("Q1Results", 0);
+          
+          window.location.href="index.html";
+      }
+    }); 
+    return false;
+  });
 
-	$("#logout").click(function() {
-		var ref = new Firebase("https://blinding-fire-5487.firebaseio.com/?page=Auth");
-		ref.unauth();
+  $("#logout").click(function() {
+    var ref = new Firebase("https://blinding-fire-5487.firebaseio.com/?page=Auth");
+    ref.unauth();
     localStorage.setItem("Greeting", "");
     console.log(localStorage.Greeting);
-		window.location.href="index.html";
-	});
+    window.location.href="index.html";
+  });
 
 
     
